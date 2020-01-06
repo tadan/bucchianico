2d71bc55dd30788efdda3ec8ff9e2ca653403bba
Daniele Tatasciore <daniele@uni-bremen.de>
2020-01-06 01:29:34 +0100

    new build added to the repo

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/danieletatasciore/bucchianiconet/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/danieletatasciore/bucchianiconet/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/danieletatasciore/bucchianiconet/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/danieletatasciore/bucchianiconet/src/pages/page-2.js")))
